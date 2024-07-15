874eacec385538ecddf53c3d8898e4775e7b7546
naruhikohama <naruhikohama@gmail.com>
2024-07-14 21:30:18 -0300

    feat: implementing link finder

--- src/main/routes/trips_routes.py
+++ src/main/routes/trips_routes.py
@@ -6,6 +6,7 @@ from src.controllers.trip_creator import TripCreator
 from src.controllers.trip_finder import TripFinder
 from src.controllers.trip_confirmer import TripConfirmer
 from src.controllers.link_creator import LinkCreator
+from src.controllers.link_finder import LinkFinder
 
 from src.models.repositories.trips_repository import TripRepository
 from src.models.repositories.links_to_repository import LinksToRepository
@@ -45,12 +46,22 @@ def confirm_trip(tripId):
 
     return jsonify(response['body']), response['status_code']
 
-@trips_routes_bp.route('/trips/<tripId>/confirm', methods=['POST'])
+@trips_routes_bp.route('/trips/<tripId>/links', methods=['POST'])
 def create_link(tripId):
     conn = db_connection_handler.get_connection()
     links_repository = LinksToRepository(conn)
     controller = LinkCreator(links_repository)
 
     response = controller.create(request.json, tripId)
+
+    return jsonify(response['body']), response['status_code']
+
+@trips_routes_bp.route('/trips/<tripId>/links', methods=['GET'])
+def find_trip_links(tripId):
+    conn = db_connection_handler.get_connection()
+    links_repository = LinksToRepository(conn)
+    controller = LinkFinder(links_repository)
+
+    response = controller.find(tripId)
 
     return jsonify(response['body']), response['status_code']
